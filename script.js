fb0c1f83ef9a2ff89cbffc5772d519aa9c3ace9f
faisal shaik <142901842+FaisalShaik007@users.noreply.github.com>
2025-01-07 17:26:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ template literals giving space between a string
 console.log("fasial\nshaik");
 
 console.log("jawad\tnazish");
+
+
+print 1 to 5
+for(let i=0; i<50; i++ ){
+     console.log("i=",i);
+
+    
+}
