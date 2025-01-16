f47cfc7ef280975c402616f36aaa18f703bc9b6b
faisal shaik <142901842+FaisalShaik007@users.noreply.github.com>
2025-01-17 00:15:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -198,3 +198,13 @@ for (let val of str) {
     
 }
 console.log("string size= ", size );
+
+
+let str="tu"
+let size=0;
+for (let i of str) {
+    console.log("i",i);
+    size++;
+
+}
+console.log("string size= ", size );
