2a6e0f39154bd4606fb05ca08204098b4a0c084a
faisal shaik <142901842+FaisalShaik007@users.noreply.github.com>
2025-02-19 23:13:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ template literals giving space between a string
 
 console.log("fasial\nshaik");
 
-console.log("jawad\tnazish")
+console.log("jawad\tnazish");
 
 template literals giving space between a string 
 
